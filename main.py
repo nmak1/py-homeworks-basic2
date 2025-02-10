9cd9b938ba48e9422fa69d80cba2ed57755111fd
nmak1 <nmak1@rambler.ru>
2025-02-10 16:44:18 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+def read_cook_book(file_path):
+    cook_book = {}
+    with open(file_path, 'r', encoding='utf-8') as file:
+        while True:
+            dish_name = file.readline().strip()
+            if not dish_name:
+                break
+            ingredient_count = int(file.readline().strip())
+            ingredients = []
+            for _ in range(ingredient_count):
+                ingredient_info = file.readline().strip().split(' | ')
+                ingredient = {
+                    'ingredient_name': ingredient_info[0],
+                    'quantity': int(ingredient_info[1]),
+                    'measure': ingredient_info[2]
+                }
+                ingredients.append(ingredient)
+            cook_book[dish_name] = ingredients
+            file.readline()  # Пропускаем пустую строку между рецептами
+    return cook_book
+
+# Пример использования
+cook_book = read_cook_book('recipes.txt')
+print(cook_book)
