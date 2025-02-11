651feedb478d3afc648d36af6e2665d6f09cd8d7
nmak1 <nmak1@rambler.ru>
2025-02-11 10:39:51 +0300

    change sum functions

--- main.py
+++ main.py
@@ -1,85 +1,101 @@
 import json
+from typing import Any
 
 
-def read_cook_book(file_path):
-    cook_book = {}
-    with open(file_path, 'r', encoding='utf-8') as file:
-        while True:
-            dish_name = file.readline().strip()
-            if not dish_name:
-                break
-            ingredient_count = int(file.readline().strip())
-            ingredients = []
-            for _ in range(ingredient_count):
-                ingredient_info = file.readline().strip().split(' | ')
-                ingredient = {
-                    'ingredient_name': ingredient_info[0],
-                    'quantity': int(ingredient_info[1]),
-                    'measure': ingredient_info[2]
-                }
-                ingredients.append(ingredient)
-            cook_book[dish_name] = ingredients
-            file.readline()  # Пропускаем пустую строку между рецептами
-    return cook_book
+class CookBook:
+    def __init__(self, path):
+        self.path = path
+
+    def __str__(self):
+        return self.path
+
+    def read_cook_book(file_path) :
+        cook_book = {}
+        with open(str(file_path), 'r', encoding='utf-8') as file:
+            while True:
+                dish_name = file.readline().strip()
+                if not dish_name:
+                    break
+                ingredient_count = int(file.readline().strip())
+                ingredients = []
+                for _ in range(ingredient_count):
+                    ingredient_info = file.readline().strip().split(' | ')
+                    ingredient = {
+                        'ingredient_name': ingredient_info[0],
+                        'quantity': int(ingredient_info[1]),
+                        'measure': ingredient_info[2]
+                    }
+                    ingredients.append(ingredient)
+                cook_book[dish_name] = ingredients
+                file.readline()  # Пропускаем пустую строку между рецептами
+        return cook_book
 
 
-def get_shop_list_by_dishes(dishes, person_count, cook_book):
-    shop_list = {}
-    for dish in dishes:
-        if dish in cook_book:
-            for ingredient in cook_book[dish]:
-                ingredient_name = ingredient['ingredient_name']
-                measure = ingredient['measure']
-                quantity = ingredient['quantity'] * person_count
-                if ingredient_name in shop_list:
-                    shop_list[ingredient_name]['quantity'] += quantity
-                else:
-                    shop_list[ingredient_name] = {'measure': measure, 'quantity': quantity}
-    return shop_list
-def save_book(book, file_path):
+    def get_shop_list_by_dishes(dishes, person_count, cook_book):
+        shop_list = {}
+        for dish in dishes:
+            if dish in cook_book:
+                for ingredient in cook_book[dish]:
+                    ingredient_name = ingredient['ingredient_name']
+                    measure = ingredient['measure']
+                    quantity = ingredient['quantity'] * person_count
+                    if ingredient_name in shop_list:
+                        shop_list[ingredient_name]['quantity'] += quantity
+                    else:
+                        shop_list[ingredient_name] = {'measure': measure, 'quantity': quantity}
+        return shop_list
 
-    with open(file_path, 'w', encoding='utf-8') as file:
-        json.dump(book, file, ensure_ascii=False, indent=4)
+class OutPut:
+    def __init__(self, path):
+        self.path = path
 
-def load_book(file_path):
+    def __str__(self):
+        return self.path
 
-    with open(file_path, 'r', encoding='utf-8') as file:
-        return json.load(file)
+    def save_book(self, file_path):
+
+        with open(str(file_path), 'w', encoding='utf-8') as file:
+            json.dump(self, file, ensure_ascii=False, indent=4)
+
+    def load_book(file_path) -> Any:
+
+        with open(str(file_path), 'r', encoding='utf-8') as file:
+            return json.load(file)
 
 
-def print_book(book):
+    def print_book(self):
 
-    print(json.dumps(cook_book, ensure_ascii=False, indent=4))
+        print(json.dumps(self, ensure_ascii=False, indent=4))
 
 
-
-def merge_files(file_names, output_file):
-    files_info = []
-    for file_name in file_names:
-        with open(file_name, 'r', encoding='utf-8') as file:
-            lines = file.readlines()
-            files_info.append({
-                'name': file_name,
-                'line_count': len(lines),
-                'content': lines
-            })
-    files_info.sort(key=lambda x: x['line_count'])
-    with open(output_file, 'w', encoding='utf-8') as output:
-        for file_info in files_info:
-            output.write(f"{file_info['name']}\n{file_info['line_count']}\n")
-            output.writelines(file_info['content'])
+class MergeFiles:
+    def merge_files(file_names, output_file) :
+        files_info = []
+        for file_name in file_names:
+            with open(file_name, 'r', encoding='utf-8') as file:
+                lines = file.readlines()
+                files_info.append({
+                    'name': file_name,
+                    'line_count': len(lines),
+                    'content': lines
+                })
+        files_info.sort(key=lambda x: x['line_count'])
+        with open(output_file, 'w', encoding='utf-8') as output:
+            for file_info in files_info:
+                output.write(f"{file_info['name']}\n{file_info['line_count']}\n")
+                output.writelines(file_info['content'])
 
 # Пример использования
 if __name__ == "__main__":
-    cook_book = read_cook_book('recipes.txt')
-    save_book(cook_book, 'cook_book.json')  # Сохраняем в JSON
-    cook_book = load_book('cook_book.json')  # Загружаем из JSON
-    print_book(cook_book)  # Красивый вывод
+    cook_book = CookBook.read_cook_book('recipes.txt')
+    OutPut.save_book(cook_book, 'cook_book.json')  # Сохраняем в JSON
+    cook_book = OutPut.load_book('cook_book.json')  # Загружаем из JSON
+    OutPut.print_book(cook_book)  # Красивый вывод
 
-    shop_list = get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2, cook_book)
-    save_book(shop_list, 'cook_book.json')  # Сохраняем в JSON
-    cook_book = load_book('cook_book.json')  # Загружаем из JSON
-    print_book(shop_list)
+    shop_list =CookBook.get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2, cook_book)
+    OutPut.save_book(shop_list, 'shop_list.json')  # Сохраняем в JSON
+    shop_list = OutPut.load_book('shop_list.json')  # Загружаем из JSON
+    OutPut.print_book(shop_list)
 
     file_names = ['1.txt', '2.txt']
-    merge_files(file_names, 'merged_file.txt')
+    MergeFiles.merge_files(file_names, 'merged_file.txt')
